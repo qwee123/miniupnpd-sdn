miniupnp/miniupnp

This commit fixes IPv4 and adds IPv6 pinhole to nftables.
Signed-off-by: Sven Auhagen <sven.auhagen@voleatech.de>

miniupnpd/netfilter_nft/scripts/nft_flush.sh
@@ -1,5 +1,5 @@
 #! /sbin/nft -f
 
-flush chain ip nat miniupnpd
-flush chain ip nat miniupnpd-pcp-peer
-flush chain ip filter miniupnpd
+flush chain ip nat MINIUPNPD
+flush chain ip nat MINIUPNPD-POSTROUTING
+flush chain ip filter MINIUPNPD

qwee123/miniupnpd-sdn

netfilter_nft/scripts/nft_flush.sh
@@ -1,5 +1,5 @@
 #! /sbin/nft -f
 
-flush chain ip nat miniupnpd
-flush chain ip nat miniupnpd-pcp-peer
-flush chain ip filter miniupnpd
+flush chain ip nat MINIUPNPD
+flush chain ip nat MINIUPNPD-POSTROUTING
+flush chain inet filter MINIUPNPD