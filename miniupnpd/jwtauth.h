eb6e13afd8b71196b2ad67a57c8dbd1d971515b6
ben <ben13057991@gmail>
2021-05-24 21:09:53 +0800

    finish basic parsing of the token

--- miniupnpd/jwtauth.h
+++ miniupnpd/jwtauth.h
@@ -5,10 +5,34 @@
 
 #ifdef USE_JWT_AUTH
 
+struct PortRange {
+    unsigned short start;
+    unsigned short end;
+};
+
+struct IpRange {
+    uint32_t address;
+    uint32_t mask;
+};
+
+struct Permission {
+    struct PortRange * pub_port_range;
+    unsigned int pub_port_range_len;
+    struct IpRange * int_ip_range;
+    unsigned int int_ip_range_len;
+};
+
 int
 VerifyAuthTokenAndSignature(const char* auth, int auth_len,
                 const char* sig, int sig_len,
-                const char* http_content, int http_content_len, const char * action);
+                const char* http_content, int http_content_len,
+                struct Permission * perm);
+
+struct Permission *
+CreatePermissionObject(void);
+
+void
+DestroyPermissionObject(struct Permission * perm);
 
 #endif
 
